befac780cc534caaf489d0d1f64f2f21a0df8c09
Neeltje de Jong <lieseneeltjedejong@gmail.com>
2024-03-07 20:53:53 +0100

    aboutpage af

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Image from "next/image";
+import definitie from "../../../../public/mi_nedefinitie.jpg"
+import Link from "next/link";
 
 const Page = () => {
   return (
     <>
-      <div className="flex flex-row gap-10 col-12 m-10">
-        <div className="flex col-4 "> 
+      <div className="flex flex-row gap-10 col-12">
+        <div className="flex col-5 pl-5 "> 
           Begonnen in 2020 als modemerk, genaamd mi_ne (fonetisch: /’mini’/),
           onderzochten mies raadgever en neeltje de jong meer dan alleen
           kleding. Vanuit hier ontstond mi_ne movement; een interdisciplinair collectief.<br /><br />
@@ -20,19 +22,19 @@ const Page = () => {
           Op de persoonlijke pagina’s vind je meer informatie over hun eigen
           praktijk en kwaliteiten.
         </div>
-        <div className="flex col-4">
-          <Image
+        <div className="flex col-5">
+        <Image
             width={500}
             height={350}
-            src="/mine2/public/mi_nedefinitie.jpg"
+            src={definitie}
             alt="mi_nedefinitie"
           ></Image>
         </div>
       </div>
-      <div className="flex flex-row gap-10 col-12 m-10">
-        <div>
-          <p className="flex-row col-12 font-medium text-4xl mb-3 mt-10 pl-5">  /MI / - Mies Raadgever </p>
-          <p className="flex-row col-12 pl-5 "> 
+      <div className="flex flex-row gap-10 col-12">
+        <div className="flex col-5 flex-column">
+          <p className="font-medium text-4xl mb-3 mt-10 pl-7">  /MI / - Mies Raadgever </p>
+          <p className="pl-7 "> 
           Mies werkt als beeldend kunstenaar en ontwerper. <br /><br />
           
           ’Opgeleid als grafisch ontwerper begon ik met het abstract verbeelden van concepten.
@@ -40,7 +42,7 @@ const Page = () => {
           scheidingslijn en grenzen tussen digitaal en analoog werk ontdekt. <br /><br />
 
           Door analoog werk te vermengen met de mogelijkheden van digitale
-          media, vertel ik mijn verhaal in een eigen handschrift. <br /><br />
+          media, vertel ik mijn verhaal in een eigen handschrift.
           
           Mijn werk is figuratief maar de boodschap kan abstract zijn. Hierdoor laat ik veel
           ruimte over voor eigen interpretatie. De toeschouwer kan ieder een
@@ -48,23 +50,11 @@ const Page = () => {
           
           Ik werk graag samen of in opdracht van anderen. Samen creëren is voor mij
           belangrijk. Door meerdere disciplines en visies samen te brengen komt
-          er een werk uit wat op meerdere facetten interessant kan zijn.’
-          <br /> <br />
-          <i>@miesraadgever</i> <br />
-          <i>mjraadgever@gmail.com</i>
-          <br /> <br />
-          Nomination Kazerne Design Award, Eindhoven 2020 <br />
-          Blauwdruk, Amersfoort 2020 <br />
-          Culturele Zaterdag, Utrecht 2020 <br />
-          A0 Expo, Utrecht 2020 <br />
-          Graduation Show Design Academy Eindhoven, 2019 <br />
-          Amersfoortste Prachtnacht, 2018
-          <br /> </p>
+          er een werk uit wat op meerdere facetten interessant kan zijn.’ </p>
         </div>
-        <div>
-          <p className="flex-row col-10 font-medium text-4xl mb-3 mt-10 pl-7"> / NE/ -Neeltje de Jong </p>
-          <div>
-            <p className="flex-row col-8 pl-7">
+        <div className="flex col-5 flex-column">
+          <p className="font-medium text-4xl mb-3 mt-10 pl-7"> / NE/ -Neeltje de Jong </p>
+            <p className="pl-7">
               Neeltje werkt als autonoom beeldend kunstenaar met een achtergrond
               in dans, textiel en modeontwerp. Haar melancholisch-poëtische
               handschrift is te herkennen in haar verschillende uitingsvormen.{" "}
@@ -85,28 +75,46 @@ const Page = () => {
               Ik geniet ervan om verhalen te vertellen. Om uitdrukking te geven
               aan het mythische, het ongedefinieerde, het vage, het
               onsamenhangende, het amorfe, het rauwe, het trieste en het
-              kleine.’ <br />
-              <br />
-              <i>nelaneeltje.blogspot.com</i> <br />
+              kleine.’
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-row gap-10 col-12 mb-5">
+        <div className="flex col-5 flex-column">
+          <div className="text-sm pl-7 "> 
+          <i><a href={"http://no-souls.blogspot.com/"} target="_blank" className={"hover:opacity-50"}>no-souls.blogspot.com</a> </i> <br />
+          <i>@miesraadgever</i> <br />
+          <i>mjraadgever@gmail.com</i>
+          <br /> <br />
+          <p className="leading-6">
+          Nomination Kazerne Design Award, Eindhoven 2020 <br />
+          Blauwdruk, Amersfoort 2020 <br />
+          Culturele Zaterdag, Utrecht 2020 <br />
+          A0 Expo, Utrecht 2020 <br />
+          Graduation Show Design Academy Eindhoven, 2019 <br />
+          Amersfoortste Prachtnacht, 2018 </p> </div>
+        </div>
+        <div className="flex col-6 flex-column">
+            <div className="text-sm pl-7">
+              <i><a href={"http://www.nelaneeltje.blogspot.com"} target="_blank" className={"hover:opacity-50"}>nelaneeltje.blogspot.com</a> </i> <br />
               <i>@nelaneeltje</i> <br />
               <i>nelaneeltje@gmail.com</i>
               <br /> <br />
+              <p className="leading-6">
               Publicatie en vertoning korte film SHE ‘OTW te Historische kelders
               Arnhem, 2022 <br />
               Workshop ‘waterbraidings’ te Historische kelders Arnhem, 2022{" "}
               <br />
-              Videoperformance ‘Kroning van een weesmeisje’ geschreven door
+              Videoperformance ‘Kroning van een weesmeisje’ voor
               collectief Painted Series te Amsterdam, 2022
               <br />
-              Performance ‘ChildhoodStories’ geschreven door Maria Vogt te Café
+              Performance ‘ChildhoodStories’ voor Maria Vogt te Café
               Bosh Arnhem, 2021 <br />
               Expositie ‘blaadjesromance’ en ‘droom archief’ te Nijverheid
               Utrecht, 2018 <br />
-              Mode collectie ‘shanti’ te Wilminktheater Enschede, 2016
-            </p>
+              Mode collectie ‘shanti’ te Wilminktheater Enschede, 2016</p> </div>
           </div>
         </div>
-      </div>
     </>
   );
 };
