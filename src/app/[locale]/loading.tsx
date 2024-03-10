6293126122bc88e4d4fdf47677429ded89ce15fa
miesraadgever <mj.raadgever@student.han.nl>
2024-03-10 15:56:02 +0100

    wip

---
+++ src/app/[locale]/loading.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "../../../public/7.jpeg"
+import Image from "next/image";
+
+
+
+const Loading = () => {
+  return (
+      <div className={"flex mx-auto justify-center align-middle mt-10 opacity-50"}>
+        <Image src={logo} width={300} height={200} alt={"loading"}/>
+      </div>
+  )
+
+};
+
+export default Loading;
--- src/components/Cloudinary-image.tsx
+++ src/components/Cloudinary-image.tsx
@@ -3,20 +3,25 @@
 import React from "react";
 import { CldImage } from "next-cloudinary";
 import Link from "next/link";
+import { Size, useWindowSize } from "@/lib/hooks/useWindowsize";
 
 const CloudinaryImage = (props: any) => {
+  const windowSize: Size = useWindowSize();
+  const smallScreen = windowSize.width! < 445;
+
   return (
     <div className="flex flex-col">
-      <Link href={props.title}>
-        <CldImage className={`object-cover h-${props.cover} w-${props.cover}`} {...props} />
+      <Link href={props.caption}>
+        <CldImage
+          className={`object-cover ${smallScreen ? "w-24 h-24" : "w-48 h-48"}`}
+          {...props}
+        />
       </Link>
-      <div className={`flex flex-row w-${props.cover} justify-between`}>
-        <div>
-            <Link href={props.title}>{props.title}</Link>
-        </div>
-        <div>
-          {props.alt}
-        </div>
+      <div
+        className={`flex flex-row ${smallScreen ? "w-24" : "w-48"} justify-between font-light text-sm`}
+      >
+        <Link href={props.caption}>{props.title}</Link>
+        <div>{props.alt}</div>
       </div>
     </div>
   );
