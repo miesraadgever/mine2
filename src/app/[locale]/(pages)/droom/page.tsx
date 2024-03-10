092e7793bd27f63d1459ce7cf1c1250f4120217b
Neeltje de Jong <lieseneeltjedejong@gmail.com>
2024-03-10 16:23:53 +0100

    opzet

---
+++ src/app/[locale]/(pages)/droom/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div className="flex flex-row gap-10 col-12 m-10">
+        <div className="flex col-4 ">
+         Droom Archief 
+        </div>
+            
+        </div>
+    );
+};
+
+export default page;
