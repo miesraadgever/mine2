8e33d923afe9bb49fb08d4bec18a4b6e08b84f5e
miesraadgever <mj.raadgever@student.han.nl>
2024-03-28 22:29:08 +0100

    opzet paginas

--- src/app/[locale]/(pages)/lostbooks/page.tsx
+++ src/app/[locale]/(pages)/lostbooks/page.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import { useI18n } from "../../../../../translations/client";
 import Image from "next/image";
 import { useWindowSize } from "@/lib/hooks/useWindowsize";
+import {CldImage} from "next-cloudinary";
 
 const Page = () => {
   const t = useI18n();
@@ -11,59 +12,77 @@ const Page = () => {
   const smallScreen = windowSize.width! < 700;
 
   return (
-    <div
-      className={`flex ${smallScreen ? "flex-col " : "flex-row"} columns-2 gap-10`}
-    >
-      <div
-        className={`flex-column grow-0 ${smallScreen ? "w-[100] " : "w-[35%]"}  ps-10 pt-10`}
-      >
-        <h3 className="font-medium text-xl">LOST BOOKS</h3>
-        <p className="mt-3">
-          {t("lostbooks.caption")}
-          <br></br>
-          {t("general.credits")} <i>{t("general.mies")}</i>
-          <br></br>
-          <br></br>
-          {t("general.ongoing")}
-        </p>
+      <div className={`flex ${smallScreen ? "flex-col" : "flex-row"} col-12 `}>
+        <div className={`flex flex-col ${smallScreen ? "col-12" : "col-6"}`}>
+          {/* Text */}
+          <div className="p-4 mb-4">
+            <h3 className="font-medium text-xl">LOST BOOKS</h3>
+            <p className="mt-3">
+              {t("lostbooks.caption")}
+              <br></br>
+              {t("general.credits")} <i>{t("general.mies")}</i>
+              <br></br>
+              <br></br>
+              {t("general.ongoing")}
+            </p>
+          </div>
+          {smallScreen && (
+              <div className={"col-12 justify-center flex"}>
+                <div className="flex ">
+                  <Image
+                      src={"https://res.cloudinary.com/dwgsproch/image/upload/v1710077619/MIES%20-%20lost%20books/4_wzh3pz.jpg"}
+                      alt={"book1"}
+                      width={600}
+                      height={700}
+                  />
+                </div>
+              </div>
+          )}
+          {/* Images */}
+          <div className={`flex mt-10 ${smallScreen ? "ml-0" : "ml-4"} gap-2`}>
+            <div className="">
+              <Image
+                  src={
+                    "https://res.cloudinary.com/dwgsproch/image/upload/v1710077611/MIES%20-%20lost%20books/7_vszbyq.jpg"
+                  }
+                  alt={"book1"}
+                  width={300}
+                  height={400}
+              />
+            </div>
+            <div className="">
+              <Image
+                  src={"https://res.cloudinary.com/dwgsproch/image/upload/v1710077617/MIES%20-%20lost%20books/1_qgclg2.jpg"}
+                  alt={"book2"}
+                  width={300}
+                  height={400}
+              />
+            </div>
+            <div className="">
+              <Image
+                  src={"https://res.cloudinary.com/dwgsproch/image/upload/v1710077619/MIES%20-%20lost%20books/4_wzh3pz.jpg"}
+                  alt={"book3"}
+                  width={300}
+                  height={400}
+              />
+            </div>
+          </div>
+          {/* Second row if needed */}
+          <div className="flex mt-10 ml-4 gap-3">
+            <div className="flex-1">
+            </div>
+          </div>
+        </div>
+        {!smallScreen && (
+            <div className={`fixed right-0 justify-end flex col-6`}>
+              <img
+                  src={"https://res.cloudinary.com/dwgsproch/image/upload/v1710077616/MIES%20-%20lost%20books/5_clqr0i.jpg"}
+                  alt={"book1"}
+                  className={"h-auto max-h-screen w-auto justify-end"}
+              />
+            </div>
+        )}
       </div>
-      <div
-        className={`flex flex-wrap w-[100%] gap-2 pt-10 ${smallScreen && "justify-center"}`}
-      >
-        <Image
-          src={
-            "https://res.cloudinary.com/dwgsproch/image/upload/v1710077611/MIES%20-%20lost%20books/7_vszbyq.jpg"
-          }
-          alt={"book1"}
-          width={300}
-          height={400}
-        />
-        <Image
-          src={
-            "https://res.cloudinary.com/dwgsproch/image/upload/v1710077616/MIES%20-%20lost%20books/5_clqr0i.jpg"
-          }
-          alt={"book1"}
-          width={300}
-          height={400}
-        />
-        <Image
-          src={
-            "https://res.cloudinary.com/dwgsproch/image/upload/v1710077617/MIES%20-%20lost%20books/1_qgclg2.jpg"
-          }
-          alt={"book1"}
-          width={300}
-          height={400}
-        />
-        <Image
-          src={
-            "https://res.cloudinary.com/dwgsproch/image/upload/v1710077619/MIES%20-%20lost%20books/4_wzh3pz.jpg"
-          }
-          alt={"book1"}
-          width={300}
-          height={400}
-        />
-      </div>
-    </div>
   );
 };
 
--- src/app/[locale]/(pages)/streets/page.tsx
+++ src/app/[locale]/(pages)/streets/page.tsx
@@ -4,80 +4,85 @@ import React from "react";
 import { useI18n } from "../../../../../translations/client";
 import Image from "next/image";
 import { useWindowSize } from "@/lib/hooks/useWindowsize";
+import {CldImage} from "next-cloudinary";
 
 const Page = () => {
-  const t = useI18n();
-  const windowSize = useWindowSize();
-  const smallScreen = windowSize.width! < 1010;
+    const t = useI18n();
+    const windowSize = useWindowSize();
+    const smallScreen = windowSize.width! < 800;
 
-  return (
-    <div className="flex">
-      {/* Left Column */}
-      <div className="flex-1 overflow-auto">
-        {/* Text */}
-        <div className="bg-gray-200 p-4 mb-4">
-          <h3 className="font-medium text-xl">LOST BOOKS</h3>
-          <p className="mt-3">
-            {t("lostbooks.caption")}
-            <br></br>
-            {t("general.credits")} <i>{t("general.mies")}</i>
-            <br></br>
-            <br></br>
-            {t("general.ongoing")}
-          </p>
+    return (
+        <div className={`flex ${smallScreen ? "flex-col" : "flex-row"} col-12 `}>
+           <div className={`flex flex-col ${smallScreen ? "col-12" : "col-6"}`}>
+               {/* Text */}
+               <div className="p-6 mb-4">
+                   <h3 className="font-medium text-xl">ON THE STREETS</h3>
+                   <p className="mt-3">
+                       {t("general.ongoing")}
+                   </p>
+               </div>
+               {smallScreen && (
+                   <div className={"col-12 justify-center flex"}>
+                       <div className="flex ">
+                           <Image
+                               src={"https://res.cloudinary.com/dwgsproch/image/upload/v1710076526/27.1%20MI_NE%20-%20streets%202020%207/9d02e4c3-4ef9-4b14-95e6-454c19456a74_diejyq.jpg"}
+                               alt={"street1"}
+                               width={600}
+                               height={700}
+                           />
+                       </div>
+                   </div>
+               )}
+                       {/* Images */}
+                       <div className={`flex-wrap flex gap-3 mt-10 ${smallScreen ? "ml-4" : "ml-6 justify-start"}`}>
+                               <CldImage
+                                   src={"https://res.cloudinary.com/dwgsproch/image/upload/v1710076528/27.1%20MI_NE%20-%20streets%202020%207/MN_streets_dx7pnn.jpg"}
+                                   alt={"book1"}
+                                   width={300}
+                                   height={400}
+                                   className="w-72 h-80 object-cover"
+                               />
+                               <CldImage
+                                   src={"https://res.cloudinary.com/dwgsproch/image/upload/v1710076528/27.1%20MI_NE%20-%20streets%202020%207/6ab039cf-fd09-4204-8519-ac56535ce442_-_1_sf21qg.jpg"}
+                                   alt={"book2"}
+                                   width={300}
+                                   height={400}
+                                   className="w-72 h-80 object-cover"
+                               />
+                               <CldImage
+                                   src={"https://res.cloudinary.com/dwgsproch/image/upload/v1710076527/27.1%20MI_NE%20-%20streets%202020%207/ed3be878-5ed8-4a56-b13e-d0ede80736a8_mudxvm.jpg"}
+                                   alt={"book2"}
+                                   width={300}
+                                   height={400}
+                                   className="w-72 h-80 object-cover"
+                               />
+                               <CldImage
+                                   src={"https://res.cloudinary.com/dwgsproch/image/upload/v1711641356/27.1%20MI_NE%20-%20streets%202020%207/IMG_4339_rafeyk.heic"}
+                                   alt={"book2"}
+                                   width={300}
+                                   height={400}
+                                   className="w-72 h-80 object-cover"
+                               />
+                           <CldImage
+                               src={"https://res.cloudinary.com/dwgsproch/image/upload/v1711641446/27.1%20MI_NE%20-%20streets%202020%207/preview_vesday.jpg"}
+                               alt={"book2"}
+                               width={300}
+                               height={400}
+                               className="w-72 h-80 object-cover"
+                           />
+                       </div>
+           </div>
+            {!smallScreen && (
+                    <div className={`fixed right-0 justify-end flex col-6 p-0`}>
+                        <img
+                            src={"https://res.cloudinary.com/dwgsproch/image/upload/v1710076526/27.1%20MI_NE%20-%20streets%202020%207/9d02e4c3-4ef9-4b14-95e6-454c19456a74_diejyq.jpg"}
+                            alt={"book1"}
+                            className={"h-auto max-h-screen w-auto justify-end"}
+                        />
+                    </div>
+            )}
         </div>
-        {/* Images */}
-        <div className={`flex mt-10 ml-4 gap-3`}>
-          <div className="flex-1 flex">
-            <Image
-              src={
-                "https://res.cloudinary.com/dwgsproch/image/upload/v1710077611/MIES%20-%20lost%20books/7_vszbyq.jpg"
-              }
-              alt={"book1"}
-              width={300}
-              height={400}
-            />
-          </div>
-          <div className="flex-1 ml-2">
-            <Image
-              src={
-                "https://res.cloudinary.com/dwgsproch/image/upload/v1710077616/MIES%20-%20lost%20books/5_clqr0i.jpg"
-              }
-              alt={"book1"}
-              width={300}
-              height={400}
-            />
-          </div>
-            <div className="flex-1 ml-2">
-                <Image
-                    src={"https://res.cloudinary.com/dwgsproch/image/upload/v1710077619/MIES%20-%20lost%20books/4_wzh3pz.jpg"}
-                    alt={"book1"}
-                    width={300}
-                    height={400}
-                />
-            </div>
-        </div>
-          {/* Second row */}
-          <div className="flex mt-10 ml-4 gap-3">
-              <div className="flex-1 ml-2">
-              </div>
-          </div>
-      </div>
-
-      {/* Right Column */}
-      <div className="flex-1 flex justify-end ">
-        {/* Big Column */}
-        <div className="right-0 fixed">
-          <Image
-            src={"https://res.cloudinary.com/dwgsproch/image/upload/v1710077616/MIES%20-%20lost%20books/5_clqr0i.jpg"}
-            alt={"book1"}
-            width={500}
-            height={600}
-          />
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Page;
